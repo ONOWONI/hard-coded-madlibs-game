cca22a90c777cec6f653d05bda68a09d4cfb970f
Onowoni <danielonowoni1@gmail.com>
2022-07-13 20:05:45 +0100

    hard coded madlibs game

---
+++ hello.js
@@ -0,0 +1,11 @@
+// hard coded madlibs game
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+    var result = "";
+
+    result += "The " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb
+    return result
+}
+
+
+console.log(wordBlanks("Badtnigga", "cool", "eat", "coolishly"));
+
